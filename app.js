37e2cbcc559bf417a1a1c3d9c607fb559868b284
Zurishaddai Temitope <temitopezurishaddai@gmail.com>
2021-10-18 19:45:21 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,20 +7,22 @@ function birthdayCheck() {
   const dateObj = new Date(inputDate);
   let birthDetails = {
     date: dateObj.getDate(),
-    month: dateObj.getMonth() + 1
+    month: dateObj.getMonth() +1,
+    year: dateObj.getFullYear()
   }
   console.log(birthDetails)
 
+  let currentYear = today.getFullYear();
   let currentMonth = today.getMonth() + 1;
   let currentDate = today.getDate();
 
   let title = document.querySelector('#title').value;
 
-  if (birthDetails.date == currentDate || birthDetails.month == currentMonth) {
-    document.write('Dear ' + title + ', Happy Birthday !')
+  if (birthDetails.date != currentDate || birthDetails.month != currentMonth && birthDetails.year != currentYear || birthDetails.year == currentYear) {
+    document.write('Dear ' + title + ', Not your Birthday yet !')
   }
   else {
-    document.write('Not your Birthday yet, ' + title + '')
+    document.write('Dear ' + title + ', Happy Birthday !')
   }
 }
 btn.addEventListener("click", birthdayCheck)
